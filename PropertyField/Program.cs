2a98bc19c3474bbcf0d8888d01acbb0bc50f3f25
Arti Gaikwad <arti.gaikwad@nuance.com>
2024-02-09 01:50:59 +0530

    Add project files.

---
+++ PropertyField/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class PrivateField
+{
+  private int _value;
+  public int Value
+{
+    get { return _value; }
+    set { _value = value; }
+  }
+}
+
+class Program
+{
+  static void Main()
+    {
+    PrivateField pf = new PrivateField();
+    pf.Value = 5;
+    Console.WriteLine(pf.Value);
+    Console.ReadLine();
+  }
+}
